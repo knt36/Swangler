218af5c78613fdfe9c5c8d4db73875eb882242f0
knt36 <kntran10@gmail.com>
2018-02-24 00:32:40 -0500

    added response codes and made styling responsive

--- src/app/components/endpoint/endpoint.component.ts
+++ src/app/components/endpoint/endpoint.component.ts
@@ -18,19 +18,25 @@ export class EndpointComponent implements OnInit {
     paramters: [
       {
         name: "page_number",
+        httpPart:'query',
         type: "integer",
+        required: true,
         desc: "The page number to get",
         value: "20"
       },
       {
         name: "page_line",
+        httpPart:'path',
         type: "integer",
+        required: true,
         desc: "The page number to get",
         value: "25"
       },
       {
         name: "word_number",
+        httpPart:'body',
         type: "integer",
+        required: true,
         desc: "The page number to get",
         value: "26"
       }
@@ -38,6 +44,20 @@ export class EndpointComponent implements OnInit {
     responseTypes: [
       "application/json",
       "soap/xml"
+    ],
+    statusCodes:[
+      {
+        code:200,
+        desc: 'Success'
+      },
+      {
+        code:400,
+        desc: 'No Auth'
+      },
+      {
+        code:500,
+        desc: 'Server sided problem?'
+      }
     ]
   };
 
@@ -58,12 +78,20 @@ export class EndpointComponent implements OnInit {
   @Output('testEndPointClicked') testEndPointClicked: EventEmitter<any> = new EventEmitter<any>();
 
   public selectedResponse = this.endpointData.responseTypes.length != 0 ? this.endpointData.responseTypes[0] : "none";
-  public parameterFields = this.endpointData.paramters.map(res =>{return(res.value)});
+  public parameterFields = {};
 
   constructor() {
+    this.initParameterFields();
   }
 
   ngOnInit() {
+  }
+
+  private initParameterFields(){
+    const params = this.endpointData.paramters;
+    for( const p in params){
+      this.parameterFields[params[p].name] = params[p].value;
+    }
   }
 
   public clickedTestEndPointBut(){
