b18893800d70cf35e2d6531317d8e95e589fa09e
Serghei Ivanov <serghei.ivanov@slyce.it>
2018-02-27 10:38:03 -0500

    param console requirements fix

--- src/app/components/param-console/param-console.component.ts
+++ src/app/components/param-console/param-console.component.ts
@@ -7,12 +7,11 @@ import {Schema} from '../../models/endpoint/endpoint.model';
   styleUrls: ['./param-console.component.scss']
 })
 export class ParamConsoleComponent implements OnInit {
-  public static readonly OPTIONAL = '(optional)';
-  public static readonly REQUIRED = '(required)';
+  public readonly OPTIONAL = '(optional)';
+  public readonly REQUIRED = '(required)';
   public Object = Object;
-  public getFieldRequirementTextFunc = ParamConsoleComponent.getFieldRequirementText;
   @Input('schema') schema: Schema;
-  public static getFieldRequirementText(isRequired: Boolean) {
+  public getFieldRequirementText(isRequired: Boolean) {
     return(isRequired ? this.REQUIRED : this.OPTIONAL);
   }
   ngOnInit() {
