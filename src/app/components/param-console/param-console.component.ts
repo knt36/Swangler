53e3be08ee2a3e15e934c061dfd97dfe039379a4
Serghei Ivanov <serghei.ivanov@slyce.it>
2018-02-28 10:47:21 -0500

    schema type fix

--- src/app/components/param-console/param-console.component.ts
+++ src/app/components/param-console/param-console.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Schema} from '../../models/endpoint/endpoint.model';
+import {Schema, ResponseSchema, RequestSchema} from '../../models/endpoint/endpoint.model';
 
 @Component({
   selector: 'app-param-console',
@@ -10,7 +10,7 @@ export class ParamConsoleComponent implements OnInit {
   public readonly OPTIONAL = '(optional)';
   public readonly REQUIRED = '(required)';
   public Object = Object;
-  @Input('schema') schema: Schema;
+  @Input('schema') schema: ResponseSchema | RequestSchema;
   public getFieldRequirementText(isRequired: Boolean) {
     return(isRequired ? this.REQUIRED : this.OPTIONAL);
   }
