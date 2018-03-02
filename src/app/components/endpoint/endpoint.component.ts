50daf6876d756dad7d23faff7f5b911d3707b768
Serghei Ivanov <serghei.ivanov@slyce.it>
2018-03-02 12:00:10 -0500

    disabled smooth scroll

--- src/app/components/endpoint/endpoint.component.ts
+++ src/app/components/endpoint/endpoint.component.ts
@@ -77,10 +77,12 @@ export class EndpointComponent implements OnInit, OnChanges, AfterViewInit {
     if ( id ) {
       const elem = document.getElementById(id);
       if (elem) {
-        this.smoothScroll(document.documentElement.scrollTop || document.body.scrollTop, elem.offsetTop);
+        window.scrollTo(0, elem.offsetTop + 56);
+        // this.smoothScroll(document.documentElement.scrollTop || document.body.scrollTop, elem.offsetTop);
       }
     } else {
-      this.smoothScroll(document.documentElement.scrollTop || document.body.scrollTop, 0);
+      window.scrollTo(0, 0 + 56);
+      // this.smoothScroll(document.documentElement.scrollTop || document.body.scrollTop, 0);
     }
   }
 
