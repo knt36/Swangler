74bac63d0e5c51fee49bd7aee36bdc49744db321
Serghei Ivanov <serghei.ivanov@slyce.it>
2018-03-06 14:48:40 -0500

    added unit tests for collapsable nav component

--- src/app/components/collapsable-nav/collapsable-nav.component.spec.ts
+++ src/app/components/collapsable-nav/collapsable-nav.component.spec.ts
@@ -1,14 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CollapsableNavComponent } from './collapsable-nav.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CollapseModule } from 'ngx-bootstrap';
+import { DebugElement, SimpleChanges, SimpleChange } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
 
 describe('CollapsableNavComponent', () => {
   let component: CollapsableNavComponent;
   let fixture: ComponentFixture<CollapsableNavComponent>;
+  let collapsableHader: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CollapsableNavComponent ]
+      declarations: [ CollapsableNavComponent ],
+      imports: [
+        RouterTestingModule,
+        CollapseModule.forRoot()
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +32,40 @@ describe('CollapsableNavComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should toggleExpand on .collapsable-nav-header click', async(() => {
+    spyOn(component, 'toggleExpand');
+    expect(component.isCollapsed).toBeTruthy();
+
+    collapsableHader = fixture.debugElement.query(By.css('.collapsable-nav-header'));
+    collapsableHader.triggerEventHandler('click', null);
+
+    expect(component.toggleExpand).toHaveBeenCalled();
+  }));
+
+
+  it('should set isCollapsed to true if Inputs tag and sectionToExpand are same', () => {
+      expect(component.isCollapsed).toBeTruthy();
+
+      component.sectionToExpand = 'test';
+      component.tag = 'test';
+      component.ngAfterContentInit();
+
+      expect(component.isCollapsed).toBeFalsy();
+    });
+
+
+  it('should change isCollapsed', () => {
+    component.tag = 'test';
+    component.isCollapsed = true;
+    expect(component.isCollapsed).toBeTruthy();
+
+    component.ngOnChanges(
+      { sectionToExpand: new SimpleChange(null, 'test', null) }
+    );
+
+    expect(component.isCollapsed).toBeFalsy();
+  });
+
 });
